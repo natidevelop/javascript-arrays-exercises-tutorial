6f6adbbe24589f558962aa7ede744c4955ac1fe2
Natali Martinez <163793240+natidevelop@users.noreply.github.com>
2025-02-17 14:03:18 +0000

    ejercicios de logica y bucles

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,7 @@
-let arr = [45,67,87,23,5,32,60];
-
-// Your code here
+let arr = [45, 67, 87, 23, 5, 32, 60];
+let resul = []
+for (let i = arr.length - 1; i >= 0; i--) {
+    let recorrido = arr[i];
+    resul.push(recorrido);
+    console.log(resul);
+}
